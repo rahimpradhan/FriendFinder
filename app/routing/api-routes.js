7e5ac688a230a1f1b7863295ea70c25b92df740a
Rahim Pradhan <rahimpradhan@gmail.com>
2017-11-10 10:23:38 -0500

    hw sub

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,10 +1,51 @@
 var tableData = require('../data/table-data.js');
 
 module.exports = function(app){
-	app.get('api/friends', function(req, res){
+	app.get('/api/friends', function(req, res){
 		res.json(tableData);
 
 	});
+
+
+	app.post('/api/friends', function(req, res){
+		var newFriend = req.body;
+
+		var userScore = newFriend.scores;
+
+		var matchFriend = '';
+		var matchImage = '';
+		var totalDifference = 1000
+
+		// Examine all existing friends in the list
+		for (var i = 0; i < tableData.length; i++) {
+			// console.log('friend = ' + JSON.stringify(friends[i]));
+
+			// Compute differenes for each question
+			var diff = 0;
+			for (var j = 0; j < userScore.length; j++) {
+				diff += Math.abs(tableData[i].scores[j] - userScore[j]);
+			}
+			// console.log('diff = ' + diff);
+
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
+				// console.log('Closest match found = ' + diff);
+				// console.log('Friend name = ' + friends[i].name);
+				// console.log('Friend image = ' + friends[i].photo);
+
+				totalDifference = diff;
+				matchFriend = tableData[i].name;
+				matchImage = tableData[i].photo;
+			}
+		}
+
+		// Add new user
+		friends.push(newFriend);
+
+		// Send appropriate response
+		res.json({status: 'OK', matchFriend: matchFriend, matchImage: matchImage});
+});
+
 }
 
 
